d56e7533cef9cfdddac095e8db6a779ff2e5e0c3
SploxFox <sjtl2004@gmail.com>
2020-08-22 23:03:04 -0500

    Initial commit

---
+++ install-everything.bat
@@ -0,0 +1,5 @@
+CALL npm i webpack webpack-cli --save-dev
+CALL npm i typescript ts-loader --save-dev
+CALL npm i react react-dom --save
+CALL npm i @types/react @types/react-dom --save
+CALL npm i @types/node
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// Before you can do anything, you must open the terminal and run 
+//    .\install-everything.bat
